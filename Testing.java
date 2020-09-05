dab2a7cdc3c4b6817ba908ae994732ba38abd98c
thekumarkode <parthwade@gmail.com>
2020-09-05 14:23:27 +0530

    Testing Git using a Java program

---
+++ Testing.java
@@ -0,0 +1,14 @@
+class Test
+{int a,b;
+    public static void main(String args[])
+    {Test obj = new Test ();
+    obj.display();
+    }
+    Test()
+    {a = 5;
+       b=10;
+    }
+    public void display()
+    {System.out.println("the value of a is =" + a + "the value of b is =" + b)}
+   }
+}
